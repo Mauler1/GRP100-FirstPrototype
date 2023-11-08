144461d0a3fe44671a9d13978fec52eb00f6f191
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2023-11-07 21:51:21 -0500

    Changed enemy spawning

--- gpr100FirstPrototype/Assets/Scripts/timer.cs
+++ gpr100FirstPrototype/Assets/Scripts/timer.cs
@@ -12,6 +12,8 @@ public class timer : MonoBehaviour
     public pause timerEnd;
     public TextMeshProUGUI timeDisp;
     private bool pauseCheck;
+    public GameObject enemy;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,14 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int xRand = Random.Range(10, 30);
+        int yRand = Random.Range(10, 30);
+        int negXRand = Random.Range(-10, -30);
+        int negYRand = Random.Range(-10, -30);
+        Vector3 additive1 = new Vector3(xRand, yRand, 0f);
+        Vector3 additive2 = new Vector3(negXRand, negYRand, 0f);
+        Vector3 additive3 = new Vector3(xRand, negYRand, 0f);
+        Vector3 additive4 = new Vector3(negXRand, yRand, 0f);
         pauseCheck = timerEnd.getPause();
         /*if(boss.Alive() == true)
          * {
@@ -41,6 +51,37 @@ public class timer : MonoBehaviour
             }
             convTime = (timeGoal / 60) / 60;
             convSec = (timeGoal / 60) % 60;
+            if(timeGoal % 60 == 0)
+            {
+                if(convTime < 1)
+                {
+                    GameObject spawned = Instantiate(enemy, spawnPoint.position + additive1, spawnPoint.rotation);
+                    GameObject spawned2 = Instantiate(enemy, spawnPoint.position + additive2, spawnPoint.rotation);
+                    GameObject spawned3 = Instantiate(enemy, spawnPoint.position + additive3, spawnPoint.rotation);
+                    GameObject spawned4 = Instantiate(enemy, spawnPoint.position + additive4, spawnPoint.rotation);
+                    GameObject spawned5 = Instantiate(enemy, spawnPoint.position + additive1, spawnPoint.rotation);
+                }
+                else if(convTime < 2)
+                {
+                    GameObject spawned = Instantiate(enemy, spawnPoint.position + additive3, spawnPoint.rotation);
+                    GameObject spawned2 = Instantiate(enemy, spawnPoint.position + additive1, spawnPoint.rotation);
+                    GameObject spawned3 = Instantiate(enemy, spawnPoint.position + additive2, spawnPoint.rotation);
+                    GameObject spawned4 = Instantiate(enemy, spawnPoint.position + additive4, spawnPoint.rotation);
+
+                }
+                else if (convTime < 3)
+                {
+                    GameObject spawned = Instantiate(enemy, spawnPoint.position + additive1, spawnPoint.rotation);
+                    GameObject spawned2 = Instantiate(enemy, spawnPoint.position + additive4, spawnPoint.rotation);
+                    GameObject spawned3 = Instantiate(enemy, spawnPoint.position + additive2, spawnPoint.rotation);
+                }
+                else
+                {
+                    GameObject spawned = Instantiate(enemy, spawnPoint.position + additive1, spawnPoint.rotation);
+                    GameObject spawned2 = Instantiate(enemy, spawnPoint.position + additive3, spawnPoint.rotation);
+                }
+            }
+            
 
             timeDisp.text = convTime.ToString() + ":" + convSec.ToString();
         }
