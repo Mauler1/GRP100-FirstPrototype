15f54ae42556649aab6bcff9d2f67c6c66f6d1bc
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2023-12-12 21:11:38 -0500

    Refined start and end screens, adjusted player interaction with screens

--- gpr100FirstPrototype/Assets/Scripts/monsterManual.cs
+++ gpr100FirstPrototype/Assets/Scripts/monsterManual.cs
@@ -20,6 +20,10 @@ public class monsterManual : MonoBehaviour
         {
             SwitchScene();
         }
+        else if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            SwitchScene();
+        }
     }
     public void SwitchScene()
     {
